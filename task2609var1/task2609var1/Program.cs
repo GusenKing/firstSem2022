3abc2254907d2e1d213d7406b944995474cbd29b
Emin <emin.sarux2004@yandex.ru>
2022-09-26 15:14:28 +0300

    Задание26_09

---
+++ task2609var1/task2609var1/Program.cs
@@ -0,0 +1,18 @@
+﻿double x = double.Parse(Console.ReadLine());
+double y = double.Parse(Console.ReadLine());
+
+string answer = "NO";
+if (y >= 0 && y <= 1 && x <= 1 && x >= 0)
+{
+    answer = "YES";
+}
+else if (x >= -1 && x <= 0 && y <= x + 1 && y >= 0)
+{
+    answer = "YES";
+}
+else if (x >= -1 && x <= 0 && y >= -1 && y <= 0
+    && x * x + y * y <= 1)
+{
+    answer = "YES";
+}
+Console.WriteLine(answer);
